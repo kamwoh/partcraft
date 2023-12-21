a06b5f645d2751fb25f991f342476a292e94f3f5
kam woh <kamwoh@gmail.com>
2023-12-21 16:13:19 +0000

    set hyperparameters used in the paper

--- src/run_sd_sup.sh
+++ src/run_sd_sup.sh
@@ -6,7 +6,7 @@ python train_dreamcreature_sd.py \
      --lr_scheduler="constant" --lr_warmup_steps=0 --seed=42 --output_dir="sd15-cub200-sup" \
      --validation_prompt="a photo of a 0:16 1:16 2:16 4:16 6:16" \
      --num_validation_images 8 --num_parts 8 --num_k_per_part 256 --filename="train.txt" \
-     --code_filename="train_caps_better_m8_k256.txt" --projection_nlayers=1 \
+     --code_filename="train_caps_better_m8_k256.txt" --projection_nlayers=3 \
      --use_templates --vector_shuffle --snr_gamma=5 \
      --attn_loss=0.01 --use_gt_label --bg_code=7 \
      --resume_from_checkpoint="latest" --mixed_precision="fp16"
